16b42b0e084aec086e9b78664e651602eaa6890b
justintsugranes <justintsugranes@proton.me>
2022-08-06 16:33:08 -0600

    internal routing update

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,33 @@
-function ProductList() {
+// Use the Link component to navigate to different routes
+// For internal routes, you only need to use an /PAGE href. For external routes, you still use a typical HTML href
+import Link from 'next/link'
+
+function ProductList({ productId = 4 }) {
   return (
     <>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <Link href='/'>
+        <a>Home</a>
+      </Link>
+      <h2>
+        <Link href='/product/1'>
+          <a>Product 1</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href='/product/2'>
+          <a>Product 2</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href='/product/3'>
+          <a>Product 3</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`/product/${productId}`}>
+          <a>Product {productId}</a>
+        </Link>
+      </h2>
     </>
   )
 }
